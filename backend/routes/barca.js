74b0e2f98c53b149642d54042c99eb3acf7d670a
Giorgia Gavioli <giorgia.gavioli.24@gmail.com>
2024-02-04 15:30:36 +0100

    adding date-fns

--- backend/routes/barca.js
+++ backend/routes/barca.js
@@ -2,14 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const controllerBarca = require('../controllers/controllerBarca')
-
 const checkRole = require('../middleware/check-role')
 
 router.post('/aggiungiBarca', controllerBarca.aggiungiBarca);
-
 router.get('/getDatiBarca', controllerBarca.getDatiBarca);
-
 router.delete('/eliminaBarca', checkRole.checkPermessiProprietarioBarca, controllerBarca.eliminaBarca);
+
+router.post('/creaPrenotazione', controllerBarca.creaPrenotazione);
+router.get('/getDatiPrenotazione', controllerBarca.getDatiPrenotazione);
+router.delete('/eliminaPrenotazione', controllerBarca.eliminaPrenotazione);
 
 
 module.exports = router
