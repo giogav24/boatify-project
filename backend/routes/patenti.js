9c125e4f5938675e0a69a9d8f922d22bb7393bf2
Giorgia Gavioli <giorgia.gavioli.24@gmail.com>
2024-02-04 12:30:21 +0100

    funzione eliminaPatente  con test richieste postman

--- backend/routes/patenti.js
+++ backend/routes/patenti.js
@@ -3,8 +3,12 @@ const router = express.Router()
 
 const controllerPatente = require('../controllers/controllerPatente')
 
+
 router.post('/registraPatente', controllerPatente.registraPatente);
 
-router.get('./getPatentiUtente', controllerPatente.getPatentiUtente);
+router.get('/getPatentiUtente', controllerPatente.getPatentiUtente);
+
+router.delete('/eliminaPatente', controllerPatente.eliminaPatente);
+
 
 module.exports = router
