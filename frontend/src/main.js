73330ceaacddef8d3a3e3ad044a3e5838fe067be
Giorgia Gavioli <giorgia.gavioli.24@gmail.com>
2024-02-12 17:46:42 +0100

    set up frontend with vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,6 @@
-import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store/index'
-
-const app = createApp(App)
-
-app.use(router)
-app.use(store)
-app.mount('#app')
+createApp(App).use(store).use(router).mount("#app");
