c9b47e0fcf3c455e27447f36ad5e97a29050870b
Giorgia Gavioli <giorgia.gavioli.24@gmail.com>
2024-02-13 22:33:45 +0100

    allow render

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.devServer
+      .allowedHosts([
+        'localhost',
+        'boatifyappfrontend.onrender.com',
+      ]);
+  },
 });
